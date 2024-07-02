bd7f310c015739e53b4e8c5c432771cf1fccb81e
Prem Jadwani <pjadwani_be21@thapar.edu>
2024-07-03 00:57:40 +0530

    Update Login.js

--- client/src/pages/Admin/Login.js
+++ client/src/pages/Admin/Login.js
@@ -18,7 +18,7 @@ function Login() {
     const login = async ()=>{
         try {
             dispatch(ShowLoading());
-            const response=await axios.post("/api/portfolio/admin-login", user);
+            const response=await axios.post("https://mern-portfolio-server-2ft6.onrender.com/api/portfolio/admin-login", user);
             dispatch(HideLoading());
             if(response.data.success){
                 message.success(response.data.message);
